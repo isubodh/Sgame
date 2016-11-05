be0c9d60385dc87a72b1b9c2d3434e9fd928dc09
Subodh Sharma <isubodh@gmail.com>
2016-11-05 13:13:43 +0530

    Gui now looking resonably okie

--- sgame.py
+++ sgame.py
@@ -6,6 +6,11 @@ import sgameDll as dll
 
 win = tk.Tk()
 win.title("Sgame")
+frmGame = tk.Frame(win)
+frmGame.grid(column=0, row=0)
+frmCtrl = tk.Frame(win)
+frmCtrl.grid(column=0, row=1)
+
 		
 click_counts = 0
 control = [-1 for x in range(9)]
@@ -23,7 +28,6 @@ def clickMe(keynum):
 	print('In clickMe', keynum, control)
 	click_counter(1)
 	move_key(keynum)
-	#lblStart.configure(text='Number of clicks :' + str(click_counts))
 	btnReLabel()
 	verify()
 
@@ -31,6 +35,7 @@ def click_counter(count):
 	global click_counts
 	if count == 0: click_counts =0
 	else: click_counts += count
+	lblStart.configure(text='Number of clicks :' + str(click_counts))
 	
 def btn_label(x):
 	print('In btn_label', control)
@@ -50,11 +55,12 @@ def btnReLabel():
 			btnArr[var].state = 'DISABLED'
 
 def verify():
+	global click_counts
 	print('In verify', control)
 	for var in range(9):
 		if var != control[var]:
 			return
-	mBox.showinfo('Hey !congrats U Won') 
+	mBox.showinfo('Winner','Hey !congrats U Won in '+click_counts+' cliks') 
 
 def game_reset():
 	click_counter(0)
@@ -105,20 +111,27 @@ def move_key(key):
 			
 def game_exit():
 	exit()
+
+def game_about():
+	message = 'Sgame from Subodh' + '\n'
+	message += 'isubodh@gmail.com'
+	mBox.showinfo('Abt', message) 
 	
 btnArr = []
 for cntr in range(9):
-	btnArr.append(ttk.Button(win, text=str(btn_label(cntr)), command=lambda keynum=cntr:clickMe(keynum)))
+	btnArr.append(ttk.Button(frmGame, text=str(btn_label(cntr)), command=lambda keynum=cntr:clickMe(keynum)))
 	btnArr[cntr].grid(row=(cntr//3),column=(cntr%3))
 
 
-btnReset = ttk.Button(win, text='Reset', command=game_reset)
-btnReset.grid(column=0, row=3)
-btnReset = ttk.Button(win, text='Exit', command=game_exit)
-btnReset.grid(column=2, row=3)
+btnReset = ttk.Button(frmCtrl, text='Reset', command=game_reset)
+btnReset.grid(column=0, row=1)
+btnReset = ttk.Button(frmCtrl, text='Exit', command=game_exit)
+btnReset.grid(column=2, row=1)
+btnReset = ttk.Button(frmCtrl, text='About', command=game_about)
+btnReset.grid(column=1, row=1)
 
-lblStart = ttk.Label(win, text="")
-lblStart.grid(column=0, row=4)
+lblStart = ttk.Label(frmCtrl, text="")
+lblStart.grid(column=0, row=0)
 
 win.mainloop()
 
