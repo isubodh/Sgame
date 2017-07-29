dd1cda9a46ce13c730ccf958ae7486022d030fd1
Your Name <you@example.com>
2017-07-30 01:18:10 +0530

    Congrats message and start a new game on 'OK'

--- sgame.py
+++ sgame.py
@@ -7,9 +7,9 @@ class SgameGUI:
 		self.win = win
 		win.title("Sgame")
 		
-		#===========================================================================
+		'''
 		# Set up the Frame
-		#===========================================================================
+		'''
 		self.frmGame = tk.Frame(win)
 		self.frmGame.grid(column=0, row=0)
 		self.frmMsg = tk.Frame(win)
@@ -17,14 +17,14 @@ class SgameGUI:
 		self.frmCtrl = tk.Frame(win)
 		self.frmCtrl.grid(column=0, row=2)
 		
-		#===========================================================================
+		'''
 		# Set up the control variables & Click counts
-		#===========================================================================		
+		'''		
 		self.game_start()
 		
-		#===========================================================================
+		'''
 		# Set up the Button Array and position them
-		#===========================================================================		
+		'''		
 		self.btnArr = []
 		for cntr in range(9):
 			self.btnArr.append(ttk.Button(self.frmGame,
@@ -33,9 +33,9 @@ class SgameGUI:
 			self.btnArr[cntr].height = 2111
 			self.btnArr[cntr].grid(row=(cntr//3),column=(cntr%3))
 
-		#===========================================================================
+		'''
 		# Set up the Button for Reset, Exit and About
-		#===========================================================================			
+		'''			
 		self.btnReset = ttk.Button(self.frmCtrl, text='Reset', command=self.game_reset)
 		self.btnReset.grid(column=0, row=1)
 		self.btnReset = ttk.Button(self.frmCtrl, text='Exit', command=self.game_exit)
@@ -46,9 +46,9 @@ class SgameGUI:
 		self.lblStart = ttk.Label(self.frmMsg, text="")
 		self.lblStart.grid(column=0, row=0)
 		
-		#===========================================================================
+		'''
 		# Lets have menus too
-		#===========================================================================
+		'''
 		self.menuBar = Menu(win) 
 		win.config(menu=self.menuBar)
 		self.fileMenu = Menu(self.menuBar, tearoff=0) # 1
@@ -64,58 +64,59 @@ class SgameGUI:
 		self.helpMenu.add_command(label="About",command=self.game_about)
 		
 		
-#===========================================================================
+'''
 # Function to Start the Game and randomize the key labels
-#===========================================================================		
+'''		
 	def game_start(self):
+		key_count = 9
 		self.click_counts = 0
-		self.control = [-1 for x in range(9)]
-		for var in range(9):
-			place = random.randint(0,8)
+		self.control = [-1 for x in range(key_count)]
+		for var in range(key_count):
+			place = random.randint(0,key_count - 1)
 			while self.control[place] != -1:
-				if place == 8:
+				if place == key_count -1 :
 					place = 0
 				else:
 					place +=1
 			self.control[place] = var
 
-#===========================================================================
+'''
 # Function to reset the game when not able to play it anymore
-#===========================================================================			
+'''			
 	def game_reset(self):
 		self.game_start()
 		self.btnReLabel()
 		self.click_counter(0)
 
-#===========================================================================
+'''
 # Let me give my contact details 
-#===========================================================================		
+'''		
 	def game_about(self):
 		message = 'Sgame from Subodh' + '\n'
 		message += 'isubodh@gmail.com'
 		mBox.showinfo('Abt', message)
 
-#===========================================================================
+'''
 # Function to give the player some basic instructions
-#===========================================================================
+'''
 	def game_info(self):
 		message = 'The idea is to arrange' + '\n'
 		message += 'numbers 1 - 8 ' + '\n'
 		message += 'and last box as empty' + '\n'
 		mBox.showinfo('Abt', message)
 
-#===========================================================================
+'''
 # Function that runs when use clicks the game keys
-#===========================================================================
+'''
 	def clickMe(self,keynum):
 		self.click_counter(1)
 		self.move_key(keynum)
 		self.btnReLabel()
 		self.verify()
 
-#===========================================================================
+'''
 # Function to keep count of key-strokes to make player serious
-#===========================================================================		
+'''		
 	def click_counter(self,count):
 		if count == 0: 
 			self.click_counts =0
@@ -123,9 +124,9 @@ class SgameGUI:
 			self.click_counts += count
 		self.lblStart.configure(text='Number of clicks : ' + str(self.click_counts), justify='center')
 
-#===========================================================================
+'''
 # Function to relabel the keys
-#===========================================================================		
+'''		
 	def btn_label(self,x):
 		if self.control[x] != 0 :
 			return str(self.control[x])
@@ -141,19 +142,20 @@ class SgameGUI:
 				self.btnArr[var].configure(text=str(''))
 				self.btnArr[var].state = 'DISABLED'
 
-#===========================================================================
+'''
 # Function verify that the player has won, if so display a popup
-#===========================================================================	
+'''	
 	def verify(self):
 		for var in range(9):
 			if var != self.control[var]:
 				return
 		mBox.showinfo('Winner','Hey !congrats U Won in '+self.click_counts+' cliks') 
 
-#===========================================================================
-# Function to show that user clicks are moving the numbers around
-#===========================================================================		
+	
 	def move_key(self,key):
+	'''
+	# Function to show that user clicks are moving the numbers around
+	'''	
 		#global control
 		## Check Right
 		if key%3 != 2:
@@ -180,17 +182,18 @@ class SgameGUI:
 				self.control[key]=0	
 				return
 				
-#===========================================================================
-# Exit funtion and cleanup code too.
-#===========================================================================				
+				
 	def game_exit(self):
+	'''
+	# Exit funtion and cleanup code too.
+	'''
 		self.win.quit()
 		self.win.destroy()
 		exit()
 
-#===========================================================================
+'''
 # Main where the action starts.
-#===========================================================================	
+'''	
 win = tk.Tk()
 SgameGUI(win)
 win.mainloop()
